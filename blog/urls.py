a9b3bbbf74e125c0dd074f43ae6d10297c0096a7
David <nzambulidavid@gmail.com>
2024-05-05 14:48:15 +0300

    added comment approval and removal

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('post/<pk>/remove/', views.post_remove,name='post_remove'),
     path('accounts/logout/', views.custom_logout_view, name='logout'),
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
