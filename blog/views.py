69a43974a42b55558a5b3499aaf51ab2c3b07470
David <nzambulidavid@gmail.com>
2024-05-07 00:38:58 +0300

    continued switching views to generic views and added some comments for understanding

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
-from django.views.generic import ListView
+from typing import Any
+from django.db.models.query import QuerySet
+from django.urls import reverse
+from django.views.generic import ListView, DetailView,CreateView, UpdateView
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.utils import timezone
 from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import logout
 
 class PostListView(ListView):
@@ -14,42 +18,51 @@ class PostListView(ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name= 'blog/post_detail.html'
+    context_object_name = 'post'
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+class PostNewView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    form_class = PostForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user # This line sets the author attribute of the form's instance to the currently logged-in user
+        return super().form_valid(form)  # This line calls the form_valid() method of the parent class (CreateView) to perform the default behavior of saving the form data to the database. This ensures that the form data is saved correctly after the additional processing in the overridden form_valid() method.
+    
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'pk':self.object.pk})
 
 
-@login_required
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form':form})
+class PostEditView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    form_class = PostForm
+    context_object_name = 'form'
 
 
-@login_required
-def post_edit(request, pk):
-    post=get_object_or_404(Post, pk=pk)
-    if request.method=="POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form=PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form':form})
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'pk': self.object.pk})
+    
+class PostDraftList(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'blog/post_draft_list.html'
+    context_object_name = 'posts'
 
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    
 
 @login_required
 def post_draft_list(request):
